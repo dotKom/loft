10d32e901123f1ba93263183901378e89a5fbac4
Nils Herde <mail@hernil.com>
2017-05-03 21:13:59 +0200

    Initial du-checking. Needs much semantic work and prettyness

--- loft/helpers.py
+++ loft/helpers.py
@@ -1,11 +1,32 @@
 import subprocess
 
+
 def du(path):
     du = subprocess.run(["du", "-sb", path], stdout=subprocess.PIPE)
     # Decode binary string, split return and fetch first entry
     # in array which is the size in bytes
     size = du.stdout.decode().split()[0]
+    return size
 
 
-def sanity_checks(config, logger):
-    pass
+def du_remote(host, path):
+    du = subprocess.run(["ssh", host, "du", "-sb", path], stdout=subprocess.PIPE)
+    size = du.stdout.decode().split()[0]
+    return size
+
+
+def du_check(job):
+    source_size = du(job.source)
+    dest_size = du_remote(job.dest_host, job.dest_location) if job.dest_host else du(job.dest_location)
+    if source_size == dest_size:
+        return True
+    else:
+        return "Sizes are not correct %s vs %s" % (source_size, dest_size)
+
+
+def sanity_checks(job, logger):
+    if job.agent == 'rsync':
+        if du_check(job):
+            logger.info("correct size")
+        else:
+            logger.info("brrrt")
